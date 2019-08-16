e407fb75f9987756ee9898fddd7009434ccd3b36
Melanie Harris <mell.j.harris@gmail.com>
2019-08-15 20:57:52 -0700

    error fixed

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,3 +1,7 @@
+const knex = require('knex')
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development)
+
 
 module.exports = {
   find,
@@ -15,34 +19,41 @@ function find() {
 }
 
 function findById(id) {
-    return db('schemes').where({id: Number(id)});
+    return db('schemes').where({id: Number(id)}) 
 }
 
 function findSteps(id) {
-    return db("steps").were({ id: Number(id) });
+    return db("steps")
+    .InnerJoin('schemes', 'steps.scheme_id', 'schemes.id')
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where('steps.scheme_id', id)
+    .orderBy('steps.step_number')
+
 }
 
 function add(schemeData) {
-    return db("schemes")
-    .insert(schemeData)
-    .then(id =>({id: ids[0]}))
+    return db("schemes")//object
+    .insert(schemeData)//passing in shceme to database
+    .then(ids =>({id: ids[0]}))
 }
 
-function addStep(stepData, id) {
+function addStep(stepData) {
     return db("steps")
       .insert(stepData)
-      .then(id => ({ id: ids[0] }));
+      .then(ids => ({ id: ids[0] }));
 }
 
 function update(changes, id) {
     return db("schemes")
-      .where('id', Number(id))
-      .update(changes);
+      .where('id', Number(id))// finds the id
+      .update(changes);//
 }
 
 function remove(id) {
     return db("schemes")
-      .where("id", Number(id))
+      .where("id", Number(id))//finds the id (number will validate if its a number, if not a number entred it will display null)
       .del();
 }
+
+
 
